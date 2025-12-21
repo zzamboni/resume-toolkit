405c3c3e651724bb3b059380bb7fc5db5ab4fc65
Diego Zamboni <diego@zzamboni.org>
2025-12-21 22:47:26 +0100

    Use npx to run resumed
    Makes it easier to use the local cloned copy.
    Also added the local cloned copy to .gitignore.

--- scripts/render_cv.sh
+++ scripts/render_cv.sh
@@ -9,8 +9,8 @@ mkdir -p "$(dirname "$cv_html")"
 
 echo "→ Rendering CV to $cv_html"
 if [[ -n "$theme" ]]; then
-  resumed render --theme "$theme" --output "$cv_html" "$cv_json"
+  npx resumed render --theme "$theme" --output "$cv_html" "$cv_json"
 else
-  resumed render --output "$cv_html" "$cv_json"
+  npx resumed render --output "$cv_html" "$cv_json"
 fi
 
